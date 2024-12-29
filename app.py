9e90e66ae143f2ae8e50d79d27b38e309a0260cb
Pravesh Dholwani <70967219+pravesh-dholwani@users.noreply.github.com>
2024-12-29 14:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from llm_utils import driver
 song_name = st.text_input("Song Name")
 artist_name = st.text_input("Artist Name")
 music_lang = st.text_input("Music Language")
-
+os.environ["GROQ_API_KEY"] = st.secrets['GROQ_API_KEY']
 if st.button("Explain"):
     explaination = driver(song_name, artist_name, music_lang)
     st.markdown(explaination)
